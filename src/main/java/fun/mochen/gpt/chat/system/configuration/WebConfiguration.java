ed51edf7bbec66779e86d16cd7df8b8cb5c2af22
mochenya <2827075398@qq.com>
2023-11-15 14:50:05 +0800

    add base class

---
+++ src/main/java/fun/mochen/gpt/chat/system/configuration/WebConfiguration.java
@@ -0,0 +1,25 @@
+package fun.mochen.gpt.chat.system.configuration;
+
+import fun.mochen.gpt.chat.system.interceptors.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    /**
+     * 添加拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login")
+                .excludePathPatterns("/user/register");
+    }
+}
---
+++ src/main/java/fun/mochen/gpt/chat/system/interceptors/AuthInterceptor.java
@@ -0,0 +1,15 @@
+package fun.mochen.gpt.chat.system.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+}
