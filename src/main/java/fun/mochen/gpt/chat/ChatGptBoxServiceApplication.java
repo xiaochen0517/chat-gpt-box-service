3e07617dff042796c1f47b928d4302252a082766
mochenya <2827075398@qq.com>
2023-11-15 14:23:29 +0800

    add config

---
+++ src/main/java/fun/mochen/gpt/chat/ChatGptBoxServiceApplication.java
@@ -0,0 +1,13 @@
+package fun.mochen.gpt.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptBoxServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGptBoxServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/mochen/gpt/chat/ChatGptBoxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mochen.gpt.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptBoxServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
